481934f708572f93aa8c325dec6b150c55901d0e
 <96326525+mutasim77@users.noreply.github.com>
2024-09-14 06:58:41 +0500

    feat: update sha256 hash

--- wifi-password.rb
+++ wifi-password.rb
@@ -2,7 +2,7 @@ class WifiPassword < Formula
   desc "Retrieve the password of the currently connected Wi-Fi network on macOS"
   homepage "https://github.com/mutasim77/wifi-password"
   url "https://github.com/mutasim77/wifi-password/archive/v1.0.0.tar.gz"
-  sha256 "CHANGE_ME"
+  sha256 "5321657e1eed095ab98f6aa669fe66daea21aab1dc7d710319f34bd3be476e9c"
   license "MIT"
 
   def install
